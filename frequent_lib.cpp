eff248efd9b5317be2b9a32ab732f8c067f0a96d
Junyong Lee <junyong@wisc.edu>
2021-04-10 19:09:59 -1000

    Update frequent_lib.cpp

--- frequent_lib.cpp
+++ frequent_lib.cpp
@@ -14,12 +14,14 @@ struct Meeting {
 
 struct MeetingCmp {
   // order: 1 -> 2 -> 3
-  bool operator()(const Meeting& lhs, const Meeting& rhs) {
+  bool operator()(const Meeting& lhs, const Meeting& rhs) {  // <- this is greater
     return lhs.start > rhs.start;
   }
 };
 
 std::priority_queue<Meeting, std::vector<Meeting>, MeetingCmp> pq;
+std::priority_queue<int, vector<int>, std::greater<int>> pq2;  // 1 -> 2 -> 3
+std::priority_queue<int, vector<int>, std::less<int>> pq3;  // 3 -> 2 -> 1
 
 pq.emplace(Meeting(5, 10, "first"));
 pq.push(Meeting(2, 12, "b"));
